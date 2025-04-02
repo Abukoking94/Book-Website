a72f16874839025800bb6eddca2303a3166194df
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-04-02 15:01:52 +0300

    Named a variable named mappedBooks and accesed feched data if it exists

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,11 +1,12 @@
-import { , useState } from "react";
+import { useState } from "react";
 
 const Hero = () => {
   const [books, setBooks] = useState([]);
+
     const fetchBooks = async () => {
       try {
         const response = await fetch(
-          `https://www.googleapis.com/books/v1/volumes?q=`
+          `https://www.googleapis.com/books/v1/volumes?q`
         );
 
         if (!response.ok) throw new Error("Failed to fetch");
@@ -14,6 +15,25 @@ const Hero = () => {
       } catch (error) {
         console.error(error);
       }
+
+      const mappedBooks =
+          data.items?.map((item) => ({
+            id: item.id,
+            title: item.volumeInfo.title || "Untitled",
+            authors: item.volumeInfo.authors?.join(", ") || "Unknown Author",
+            categories: item.volumeInfo.categories?.join(", ") || "General",
+            rating: item.volumeInfo.averageRating || 0,
+            pageCount: item.volumeInfo.pageCount || "N/A",
+            printType: item.volumeInfo.printType || "Unknown",
+            ratingsCount: item.volumeInfo.ratingsCount || 0,
+            imageUrl:
+              item.volumeInfo.imageLinks?.thumbnail ||
+              getPlaceholder(item.volumeInfo.title),
+            description: item.volumeInfo.description || "",
+            infoLink: item.volumeInfo.infoLink || "#",
+          })) || [];
+
+        setBooks(mappedBooks);
     };
 
   fetchBooks();
